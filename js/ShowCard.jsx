9cef9dacd99a39a94298f327ddfc29d6422aaf7e
David G Chaves <davidgchaves@gmail.com>
2016-12-07 14:49:45 +0100

    Make <ShowCard> clickable (go to <Details> for the show)

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router'
 
-const ShowCard = ({ description, poster, title, year }) => (
-  <div className='show-card'>
-    <img src={`/public/img/posters/${poster}`} />
-    <div>
-      <h3>{title}</h3>
-      <h4>({year})</h4>
-      <p>{description}</p>
+const ShowCard = ({ description, imdbID, poster, title, year }) => (
+  <Link to={`/details/${imdbID}`}>
+    <div className='show-card'>
+      <img src={`/public/img/posters/${poster}`} />
+      <div>
+        <h3>{title}</h3>
+        <h4>({year})</h4>
+        <p>{description}</p>
+      </div>
     </div>
-  </div>
+  </Link>
 )
 
 const { string } = React.PropTypes
 ShowCard.propTypes = {
   description: string.isRequired,
+  imdbID: string.isRequired,
   poster: string.isRequired,
   title: string.isRequired,
   year: string.isRequired
