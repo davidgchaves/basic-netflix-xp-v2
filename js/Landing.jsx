65a4b4258e5a3f65d969ea075764807b058ce877
David G Chaves <davidgchaves@gmail.com>
2016-12-09 18:03:25 +0100

    Can read `searchTerm` from `redux`

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
 
-const Landing = () => (
+const Landing = ({ searchTerm }) => (
   <div className='landing'>
     <h1>Basic NetFlix XP v2</h1>
-    <input type='text' placeholder='Search' />
+    <input value={searchTerm} type='text' placeholder='Search' />
     <Link to='/search'>or Browse All</Link>
   </div>
 )
 
-export default Landing
+const { string } = React.PropTypes
+Landing.propTypes = {
+  searchTerm: string.isRequired
+}
+
+const mapModelToProps = ({ searchTerm }) => ({ searchTerm })
+
+export default connect(mapModelToProps)(Landing)
--- js/reducers.js
+++ js/reducers.js
@@ -1,7 +1,16 @@
-const DEFAULT_MODEL = {}
+import { SET_SEARCH_TERM } from './actions'
+
+const DEFAULT_MODEL = {
+  searchTerm: 'Search here...'
+}
+
+const setSearchTerm = (model, { searchTerm }) =>
+  Object.assign({}, model, { searchTerm })
 
 const rootReducer = (model = DEFAULT_MODEL, action) => {
   switch (action.type) {
+    case SET_SEARCH_TERM:
+      return setSearchTerm(model, action)
     default:
       return model
   }
