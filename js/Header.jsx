cac711bf5975bba034ab105f0854d48d20135042
David G Chaves <davidgchaves@gmail.com>
2016-12-09 19:33:59 +0100

    Refactor <Header> to be `redux`-aware

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,29 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { connect } from 'react-redux'
+import { setSearchTerm } from './actionCreators'
 
-const Header = ({ handleSearchTermChange, searchTerm, showSearch }) => {
-  const searchInput =
-    <input type='text' placeholder='Search'
-      onChange={handleSearchTermChange}
-      value={searchTerm} />
-  const backToSearch =
-    <h2>
-      <Link to='/search'>Back</Link>
-    </h2>
+class Header extends React.Component {
+  constructor (props) {
+    super(props)
 
-  return (
-    <header>
-      <h1><Link to='/'>Basic Netflix XP v2</Link></h1>
-      {showSearch ? searchInput : backToSearch}
-    </header>
-  )
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+  }
+
+  handleSearchTermChange (event) {
+    this.props.dispatch(setSearchTerm(event.target.value))
+  }
+
+  render () {
+    const { searchTerm, showSearch } = this.props
+    const searchInput =
+      <input type='text' placeholder='Search'
+        onChange={this.handleSearchTermChange}
+        value={searchTerm} />
+    const backToSearch =
+      <h2>
+        <Link to='/search'>Back</Link>
+      </h2>
+
+    return (
+      <header>
+        <h1><Link to='/'>Basic Netflix XP v2</Link></h1>
+        {showSearch ? searchInput : backToSearch}
+      </header>
+    )
+  }
 }
 
 const { func, bool, string } = React.PropTypes
 Header.propTypes = {
-  handleSearchTermChange: func,
-  searchTerm: string,
+  dispatch: func.isRequired,
+  searchTerm: string.isRequired,
   showSearch: bool.isRequired
 }
 
-export default Header
+const mapModelToProps = ({ searchTerm }) => ({ searchTerm })
+
+export default connect(mapModelToProps)(Header)
