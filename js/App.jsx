c96b50a437d60a30e128efcd1d75147f3090a486
David G Chaves <davidgchaves@gmail.com>
2016-12-10 12:21:38 +0100

    Get ready for Universal Rendering

--- js/App.jsx
+++ js/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Match } from 'react-router'
+import { Match } from 'react-router'
 import { Provider } from 'react-redux'
 import Landing from './Landing'
 import Search from './Search'
@@ -8,25 +8,23 @@ import store from './store'
 import preload from '../public/data'
 
 const App = () => (
-  <BrowserRouter>
-    <Provider store={store}>
-      <div className='app'>
-        <Match exactly pattern='/' component={Landing} />
-        <Match
-          pattern='/search'
-          component={props => <Search shows={preload.shows} {...props} />} />
-        <Match
-          pattern='/details/:id'
-          component={props =>
-            <Details
-              show={preload.shows.filter(s => props.params.id === s.imdbID)[0]}
-              {...props}
-              />
-          }
-          />
-      </div>
-    </Provider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <div className='app'>
+      <Match exactly pattern='/' component={Landing} />
+      <Match
+        pattern='/search'
+        component={props => <Search shows={preload.shows} {...props} />} />
+      <Match
+        pattern='/details/:id'
+        component={props =>
+          <Details
+            show={preload.shows.filter(s => props.params.id === s.imdbID)[0]}
+            {...props}
+            />
+        }
+        />
+    </div>
+  </Provider>
 )
 
 export default App
--- js/index.jsx
+++ js/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router'
 import App from './App'
-import '../public/normalize.css'
-import '../public/style.css'
 
 render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('app')
 )
